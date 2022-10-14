7ecc2a3d9282fec41d2e84aee582f69b498451b0
datageneration <datageneration1@gmail.com>
2022-10-13 19:28:33 -0500

    Add files via upload

---
+++ datascience/codes/DPR_caret01.R
@@ -0,0 +1,163 @@
+# Data Programming with R
+# install.packages(c('tidyverse','vroom','data.table' ,'caret', 'RANN', 'randomForest', 'gbm', 'xgboost', 'caretEnsemble', 'C50', 'earth', 'tictoc'))
+ptbu=c('descr','tidyverse','vroom','data.table' ,'caret', 'RANN', 'randomForest', 'gbm', 'xgboost', 'caretEnsemble', 'C50', 'earth', 'tictoc')
+install.packages(ptbu)
+lapply(ptbu, require, character.only = TRUE)
+
+# Import dataset: let different packages race
+library(tidyverse)
+tic()
+TEDS2016 <- read_csv("https://raw.githubusercontent.com/datageneration/gentlemachinelearning/master/data/TEDS2016.csv")
+toc()
+library(vroom)
+tic()
+TEDS2016 <- vroom("https://raw.githubusercontent.com/datageneration/gentlemachinelearning/master/data/TEDS2016.csv")
+toc()
+library(data.table)
+tic()
+TEDS2016 <- fread("https://raw.githubusercontent.com/datageneration/gentlemachinelearning/master/data/TEDS2016.csv")
+toc()
+
+# Structure of the dataframe
+
+str(TEDS2016)
+
+# See top 6 rows and 10 columns
+head(TEDS2016[, 1:10])
+
+# Quick examine of dv
+library(descr)
+freq(TEDS2016$votetsai)
+
+# Create the training and test datasets
+set.seed(100)
+
+# Step 1: Get row numbers for the training data
+train_rno <- createDataPartition(TEDS2016$votetsai, p=0.8, list=FALSE)
+class(train_rno)
+# Step 2: Create the training dataset
+trainTEDS <- TEDS2016[train_rno,]
+
+# Step 3: Create the test dataset
+testTEDS <- TEDS2016[-train_rno,]
+
+# Store X and Y for later use.
+x = trainTEDS[, 2:19]
+y = trainTEDS$votetsai
+
+
+# Missing values
+
+# Create the knn imputation model on the training data
+preProcess_missingdata_model <- preProcess(as.data.frame(trainTEDS), method='knnImpute')
+preProcess_missingdata_model
+
+
+# Use the imputation model to predict the values of missing data points
+# install.packages("RANN")
+library(RANN)  # required for knnInpute
+trainTEDS <- predict(preProcess_missingdata_model,trainTEDS)
+anyNA(trainTEDS)
+
+
+# One-Hot Encoding
+# Creating dummy variables is converting a categorical variable to as many binary variables as here are categories.
+dummies_model <- dummyVars(votetsai ~ ., data=trainTEDS)
+
+# Create the dummy variables using predict. The Y variable (votetsai) will not be present in trainData_mat.
+trainTEDS_mat <- predict(dummies_model, newdata = trainTEDS)
+
+# # Convert to dataframe
+trainTEDS <- data.frame(trainTEDS_mat)
+
+# # See the structure of the new dataset
+str(trainTEDS)
+
+
+preProcess_range_model <- preProcess(trainTEDS, method='range')
+trainTEDS <- predict(preProcess_range_model, newdata = trainTEDS)
+
+# Append the Y variable
+trainTEDS$votetsai <- y
+
+apply(trainTEDS[, 1:19], 2, FUN=function(x){c('min'=min(x), 'max'=max(x))})
+trainTEDS$votetsai = as.factor(trainTEDS$votetsai)
+# trellis.par.set(theme = col.whitebg(), warn = FALSE)
+featurePlot(x = trainTEDS[, 1:18], 
+            y = trainTEDS$votetsai, 
+            plot = "box",
+            strip=strip.custom(par.strip.text=list(cex=.7)),
+            scales = list(x = list(relation="free"), 
+                          y = list(relation="free")))
+
+featurePlot(x = trainTEDS[, 1:18], 
+            y = trainTEDS$votetsai, 
+            plot = "density",
+            strip=strip.custom(par.strip.text=list(cex=.7)),
+            scales = list(x = list(relation="free"), 
+                          y = list(relation="free")))
+
+set.seed(100)
+options(warn=-1)
+
+# Variable selection using Recursive Feature Elimination (rfe)
+# subsets1 <- c("dpp","taiwanese","dadmainland","econworse", "pubwelf", "south","green")
+subsets = c(1,2,7, 8, 10, 13, 16, 18)
+
+# names(trainTEDS)
+# Backward selection/Recursive Feature elimination
+ctrl <- rfeControl(functions = rfFuncs,
+                   method = "repeatedcv",
+                   repeats = 2,
+                   verbose = FALSE)
+
+# Training the model (could take a while)
+# use tictoc package to time the process
+library(tictoc)
+tic("Training the model")
+lmProfile <- rfe(x=trainTEDS[, 1:18], y=trainTEDS$votetsai,
+                 sizes = subsets,
+                 rfeControl = ctrl)
+toc()
+lmProfile
+# See available algorithms in caret
+modelnames <- paste(names(getModelInfo()), collapse=',  ')
+modelnames
+
+
+modelLookup('earth')
+
+# Set the seed for reproducibility
+set.seed(100)
+
+# Multivariate Adaptive Regression Splines (MARS)
+# Train the model using randomForest and predict on the training data itself.
+tic("earth method")
+model_earth = train(votetsai ~ ., data=trainTEDS, method='earth')
+fitted <- predict(model_earth)
+toc()
+plot(model_earth, main="Model Accuracies with MARS")
+
+varimp_earth <- varImp(model_earth)
+
+plot(varimp_earth, main="Variable Importance with MARS")
+
+# Step 1: Impute missing values 
+testTEDS2 <- predict(preProcess_missingdata_model, testTEDS)  
+
+# Step 2: Create one-hot encodings (dummy variables)
+testTEDS3 <- predict(dummies_model, testTEDS2)
+freq(testTEDS$votetsai)
+# Step 3: Transform the features to range between 0 and 1
+testTEDS4 <- predict(preProcess_range_model, testTEDS3)
+
+# View
+head(testTEDS4[, 1:10])
+
+# Predict on testData
+predicted <- predict(model_earth, testTEDS4)
+head(predicted)
+
+
+# Compute the confusion matrix
+confusionMatrix(reference = as.factor(testTEDS$votetsai), data = predicted, mode='everything', positive='Yes')
---
+++ datascience/codes/DPR_coviddata1.R
@@ -0,0 +1,93 @@
+# Data Programming with R
+# install.packages(c("vroom", "finalfit", "tidyverse", "descr", "RColorBrewer", "scales"))
+# lapply(c("vroom", "finalfit", "tidyverse", "descr", "RColorBrewer", "scales"), require, character.only = TRUE)
+library(vroom) # Fast reading in data
+library(finalfit) #for checking missingness and output visualization
+library(tidyverse)
+library(descr)
+library(RColorBrewer)
+library(scales)
+
+# Reading all real time data
+# vroom is the champion in reading github date, < 3 sec.
+owidall = vroom("https://github.com/owid/covid-19-data/blob/master/public/data/owid-covid-data.csv?raw=true")
+as.factor(owidall[,1:3])
+
+# Subset by year
+
+owid2022 = subset(owidall, format(as.Date(date),"%Y")==2022)
+owid2021 = subset(owidall, format(as.Date(date),"%Y")==2021)
+owid2020 = subset(vroom("https://github.com/owid/covid-19-data/blob/master/public/data/owid-covid-data.csv?raw=true"), format(as.Date(date),"%Y")==2020)
+owid09292022 = subset(owidall,  date == "2022-09-29")
+
+# Clean up OWID*  cases
+# Deselect cases/rows with OWID
+owidall = owidall[!grepl("^OWID", owidall$iso_code), ] 
+owid2022 = owid2022[!grepl("^OWID", owid2022$iso_code), ] 
+owid2021 = owid2021[!grepl("^OWID", owid2021$iso_code), ]
+owid2020 = owid2020[!grepl("^OWID", owid2020$iso_code), ]
+owidall$location=as.factor(owidall$location)
+
+# Subset by country/region: United States, EU, Asia
+owidus = subset(owidall, location=="United States")
+owideu = subset(owidall, continent=="Europe")
+owidasia = subset(owidall, continent=="Asia")
+
+# Get today's COVID data
+owidtoday = subset(vroom("https://github.com/owid/covid-19-data/blob/master/public/data/owid-covid-data.csv?raw=true"), date == Sys.Date())
+
+
+owidUStoday = subset(owid2022, location == "United States" & date >="2022-09-29")
+options(scipen=999) # Disable scientific notation
+par(family = "Palatino")
+format(owideu$new_cases, big.mark = ",", scientific = FALSE)
+
+# Europe data
+y = owideu$new_deaths
+x = as.Date(owideu$date)
+plot(x,y, pch=20, col="#E7298A", cex = .5, xaxt='n', xlab = "Date", ylab = "COVID Deaths in Europe (Daily)")
+axis(1, x, format(x, "%Y-%m"), cex.axis = .7, las = 3 , gap.axis =1.5, tick = FALSE)
+identify(x,y,owideu$location, ps=8, atpen=TRUE)
+
+# Asia data
+y = owidasia$new_deaths
+x = as.Date(owidasia$date)
+plot(x,y, pch=20, col="#66A61E",  cex = .5, xaxt='n', xlab = "Date", ylab = "COVID Deaths in Asia (Daily)")
+axis(1, x, format(x, "%Y-%m"), cex.axis = .7, las = 3 , gap.axis =1.5, tick = FALSE)
+identify(x,y,owidasia$location, ps=8, atpen=TRUE)
+
+# Format date
+owidall$date<-as.Date(owidall$date,format="%Y-%m-%d")
+# World: new cases
+
+plot(owidall$date,owidall$new_cases, pch = 20, cex = .3, xaxt='n')
+points(owidtw2022$date,owidtw2022$total_tests, pch = 20, cex = .5, xaxt='n', col = "firebrick")
+axis(1, owidall$date, format(owidall$date, "%Y-%m-%d"), cex.axis = .3, las = 3 )
+
+freq(owidall$continent)
+
+options(scipen=999) # No sci notation
+
+# Plot daily new cases over time
+format(owidall$new_cases, big.mark = ",", scientific = FALSE)
+ggplot(owidall, aes(date, new_cases, color = continent, alpha = new_cases)) + 
+  geom_point(size = .8) +
+  theme_bw() +
+  #  scale_colour_viridis_d(option = "plasma", direction = -1)+
+  scale_x_date(breaks = "month", date_labels= "%Y-%m", limits = as.Date(c("2020-02-01","2022-05-22"))) +
+  scale_y_continuous(labels = comma) + 
+  xlab("Date") +
+  ylab("New COVID Cases (Daily)") +
+  theme(axis.text.x = element_text(angle = 90), legend.position = c(.1, .8)) +
+  guides(alpha = "none") +
+  theme(text=element_text(size=9, family="Palatino")) +
+  scale_colour_brewer(palette = "Dark2", direction = -1)
+# Names of Dark2 palette: 
+## [1] "#1B9E77" "#D95F02" "#7570B3" "#E7298A" "#66A61E" "#E6AB02" "#A6761D"
+## [8] "#666666"
+# South America
+# North America: 7570B3
+# Asia: 66A61E
+# Europe: E7298A
+# Africa: E6AB02
+# Oceania: D95F02
